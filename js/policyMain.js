c34560be6034d4899a63edbd41391ebcaef2cf0f
Risham Dhillon <risdhillon@gmail.com>
2016-05-02 18:39:48 -0400

    heyhey

--- js/policyMain.js
+++ js/policyMain.js
@@ -11,13 +11,12 @@ $(document).ready(function() {
 });
 
 //set up bar chart
-var cmargin = {top: 10, right: 50, bottom: 50, left: 50};
-var cwidth = 500 - cmargin.left - cmargin.right,
-    cheight = 400 - cmargin.top - cmargin.bottom;
+var margin = {top: 40, right: 40, bottom: 40, left: 40};
+var cwidth = 500;
+var cheight = 400;
 
-var minimargin = {top:0, right:100, bottom:0, left:0};
-var miniWidth = 400,
-	miniHeight = 250;
+var miniWidth = 400 - margin.left - margin.right,
+	miniHeight = 300 - margin.top - margin.bottom;
 
 var miniColorScale = d3.scale.threshold()
 	.range(["#feedde", "#a63603"]);
@@ -26,22 +25,22 @@ var miniScale = (1.5*miniWidth);
 
 // Geo Projection
 var miniProjection = d3.geo.albersUsa()
-	.scale(400)
-	.translate([(miniWidth/2 - 30), miniHeight / 2]);
+	.scale(500)
+	.translate([miniWidth / 2, miniHeight / 2]);
 
 var miniPath = d3.geo.path()
 	.projection(miniProjection);
 
 var miniSvg = d3.select("#mini-map").append("svg")
-	.attr("width", miniWidth + minimargin.left + minimargin.right)
-	.attr("height", miniHeight + minimargin.top + minimargin.bottom)
-	.append("g");
+	.attr("width", miniWidth + margin.left + margin.right)
+	.attr("height", miniHeight + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-createMiniLegend();
 
 //set up scales
 var xPolicy = d3.scale.ordinal()
-	.rangeRoundBands([0, cwidth - cmargin.left],.3);
+	.rangeRoundBands([0, cwidth - margin.left],.3);
 
 var yPolicy = d3.scale.linear()
 	.range([cheight, 0]);
@@ -58,10 +57,10 @@ var yPolicyAxis = d3.svg.axis()
 //create bar chart
 var chart = d3.select("#policy-chart")
 	.append("svg")
-	.attr("width", cwidth + cmargin.right + cmargin.left)
-	.attr("height", cheight + cmargin.bottom + cmargin.top)
+	.attr("width", cwidth + margin.right + margin.left)
+	.attr("height", cheight + margin.bottom + margin.top)
 	.append("g")
-	.attr("transform", "translate(" + cmargin.left + "," + cmargin.top  + ")");
+	.attr("transform", "translate(" + margin.left + "," + margin.top  + ")");
 
 
 //make global variable
@@ -73,14 +72,15 @@ var nameById = {};
 var littleUsa;
 
 queue()
-	.defer(d3.json, "data/us-states-2.json")
+	.defer(d3.json, "data/us-states.json")
 	.defer(d3.csv, "data/datafinal.csv")
 	.defer(d3.csv, "data/statebystatepolicy.csv")
-	.await(loadPolicyData);
+	.await(loadData);
 
 //load CSV file
-function loadPolicyData(error, map, csv, statebystate) {
+function loadData(error, map, csv, statebystate) {
 	if (error) throw error;
+
 
 	statebystate.forEach(function(d){
 		d.id = +d.id;
@@ -88,6 +88,7 @@ function loadPolicyData(error, map, csv, statebystate) {
 		d.textOkay = +d.textOkay;
 		d.handheldBan = +d.handheldBan;
 		d.handheldOkay = +d.handheldOkay;
+		d.seatbeltAll = +d.seatbeltAll;
 		d.seatbeltAll = +d.seatbeltAll;
 		d.seatbeltFront = +d.seatbeltFront;
 		d.seatbeltSecondary = +d.seatbeltSecondary;
@@ -103,34 +104,36 @@ function loadPolicyData(error, map, csv, statebystate) {
 		nameById[+d.id] = d.state;
 	});
 
-	//store csv data in global variable
-	littleUsa = map;
-	policyData = csv;
-	statePoliciesData = statebystate;
-
-	chart.append("g")
-		.attr("class", "y axis")
-		.attr("transform", "translate(" + cmargin.left + ",0)")
-		.attr("id", "y-policy-axis")
-		.append("text")
-		.attr("transform", "rotate(-90)")
-		.attr("y", -40)
-		.attr("x", -6)
-		.attr("dy", ".6em")
-		.style("text-anchor", "end")
-		.text("Fatalities per 100 million vehicle miles traveled");
+		//store csv data in global variable
+		littleUsa = map;
+		policyData = csv;
+		statePoliciesData = statebystate;
 
 
-	chart.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(" + cmargin.left + "," + cheight + ")")
-		.attr("id", "x-policy-axis");
+		chart.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(" + margin.left + ",0)")
+			.attr("id", "y-policy-axis")
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", -40)
+			.attr("x", -6)
+			.attr("dy", ".6em")
+			.style("text-anchor", "end")
+			.text("Fatalities per 100 million vehicle miles traveled");
 
-	d3.selectAll("#policy-ranking-type").on("change", updateVisualization);
 
-	d3.selectAll("#selected-law").on("change", updateVisualization);
+		chart.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(" + margin.left + "," + cheight + ")")
+			.attr("id", "x-policy-axis");
 
-	updateVisualization();
+		d3.selectAll("#policy-ranking-type").on("change", updateVisualization);
+
+		d3.selectAll("#selected-law").on("change", updateVisualization);
+
+		updateVisualization();
+
 
 }
 
@@ -139,8 +142,14 @@ function updateVisualization() {
 	//the higher-level law category selection
 	dropdownSelection = (d3.select("#policy-ranking-type").node().value);
 
+	console.log(dropdownSelection);
 
-	mainSelection = (d3.select("#" + dropdownSelection).node().value);
+
+
+
+		mainSelection = (d3.select("#" + dropdownSelection).node().value);
+
+		console.log(mainSelection);
 
 	statePoliciesData.forEach(function (d) {
 		rateById[+d.id] = d[mainSelection];
@@ -165,56 +174,47 @@ function updateVisualization() {
 		.attr("d", miniPath)
 		.style("opacity", 0.9);
 
+
 	appending.exit().remove();
 
-	//set up domains
-	xPolicy.domain(policyData.map(function(d) { return d.category; }));
-	yPolicy.domain([.5, 1.5]);
-
-	//Draw axes
-	chart.selectAll("#x-policy-axis")
-		.transition()
-		.duration(800)
-		.call(xPolicyAxis);
-
-	chart.selectAll("#y-policy-axis")
-		.transition()
-		.duration(800)
-		.call(yPolicyAxis);
 
 
-	//make changing bar
-	var bars = chart.selectAll("rect")
-		.data(policyData);
+		//set up domains
+		xPolicy.domain(policyData.map(function(d) { return d.category; }));
+		yPolicy.domain([.5, 1.5]);
 
-	bars.enter()
-		.append("rect")
-		.attr("fill", "#4876af");
+		//Draw axes
+		chart.selectAll("#x-policy-axis")
+			.transition()
+			.duration(800)
+			.call(xPolicyAxis);
 
-	bars
-		.transition()
-		.duration(800)
-		.attr("class", "bars")
-		.attr("x", function(d){return cmargin.left + xPolicy(d.category);})
-		.attr("y", function (d) {
-			return yPolicy(d[mainSelection])
-		})
-		.attr("width", xPolicy.rangeBand())
-		.attr("height", function (d) {
-			return cheight - yPolicy(d[mainSelection]);
-		});
+		chart.selectAll("#y-policy-axis")
+			.transition()
+			.duration(800)
+			.call(yPolicyAxis);
 
-}
 
-// Legend for MiniMap
-function createMiniLegend(){
-  var minilegend = miniSvg.append("g").attr("id","minilegend").attr("transform","translate(345,95)");
+		//make changing bar
+		var bars = chart.selectAll("rect")
+			.data(policyData);
 
-  minilegend.append("rect").attr("fill","#4876af").attr("width",10).attr("height",10).attr("x",-5).attr("y",0)
-  minilegend.append("rect").attr("fill","#86cefa").attr("width",10).attr("height",10).attr("x",-5).attr("y",25)
+		bars.enter()
+			.append("rect")
+			.attr("fill", "#4876af");
 
-  minilegend.append("text").text("States with Selected Policy").attr("x",10).attr("y",10);
-  minilegend.append("text").text("Other States").attr("x",10).attr("y",35);
+		bars
+			.transition()
+			.duration(800)
+			.attr("class", "bars")
+			.attr("x", function(d){return margin.left + xPolicy(d.category);})
+			.attr("y", function (d) {
+				return yPolicy(d[mainSelection])
+			})
+			.attr("width", xPolicy.rangeBand())
+			.attr("height", function (d) {
+				return cheight - yPolicy(d[mainSelection]);
+			});
 
 }
 
